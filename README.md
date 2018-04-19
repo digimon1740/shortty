87bcad2e8b7fe16b2e9828d77ec501fae7793859
devsh <digimon.1740@gmail.com>
2018-04-19 22:44:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shortty
+
+
+What is the Shortty?
+---------
+
+Shortty is kind of URL-Shortener that has been based by Spring Webflux.
+It is simple, fast, light-weight and free open source software.
+
+   
---
+++ src/main/java/com/digimon/shortty/ShorttyApplication.java
@@ -0,0 +1,12 @@
+package com.digimon.shortty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorttyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorttyApplication.class, args);
+	}
+}
